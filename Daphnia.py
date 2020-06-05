6714e3b80ce92a52c2e4fee719129da47d516299
Nikolay Ukhov <ukhov@mail.ru>
2020-06-05 11:42:34 +0300

    2.36 Python minimal client

--- Daphnia.py
+++ Daphnia.py
@@ -5,26 +5,55 @@ import keyboard  # using module keyboard
 
 # creating tkinter window
 from ObserverClient import g_observer_client
+from ServerProtocol import CommonParams, EtherColor
 
 root = Tk()
 
 canvas = Canvas(width=1280, height=720, bg='black')
 canvas.pack(expand=YES, fill=BOTH)
 
-canvas.create_rectangle(0, 0, 50, 50, width=5, fill='red')
-
+# init eye color array
+eye_color_array = []
+rect_canvas_array = []
+for yy in range(CommonParams.OBSERVER_EYE_SIZE):
+    row_color = []
+    rect = []
+    for xx in range(CommonParams.OBSERVER_EYE_SIZE):
+        row_color.append(EtherColor(0, 0, 0))
+        rect.append(canvas.create_rectangle(xx*31, yy*31, xx*31+30, yy*31+30, width=5, fill='red'))
+    eye_color_array.append(row_color)
+    rect_canvas_array.append(rect)
 
 def func():
+    eye_texture = g_observer_client.get_eye_texture()
+    for yy in range(CommonParams.OBSERVER_EYE_SIZE):
+        for xx in range(CommonParams.OBSERVER_EYE_SIZE):
+            if eye_texture[yy][xx] != eye_color_array[yy][xx]:
+                cc = eye_texture[yy][xx]
+                color_rgb = (cc.m_colorR * cc.m_colorA // 256) * 65536 + (cc.m_colorG * cc.m_colorA // 256) * 256 + (cc.m_colorB * cc.m_colorA // 256)
+                hex_str = '#' + hex(color_rgb)[2:].zfill(6)
+                canvas.itemconfig(rect_canvas_array[yy][xx], fill=hex_str)
+
+
     try:  # used try so that if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed('q'):  # if key 'q' is pressed
-            canvas.create_rectangle(30, 30, 80, 80, width=0, fill='blue')
-            print('You Pressed A Key!')
+        left = keyboard.is_pressed('left')
+        g_observer_client.set_is_left(left)
+        right = keyboard.is_pressed('right')
+        g_observer_client.set_is_right(right)
+        up = keyboard.is_pressed('up')
+        g_observer_client.set_is_up(up)
+        down = keyboard.is_pressed('down')
+        g_observer_client.set_is_down(down)
+        forward = keyboard.is_pressed('space')
+        g_observer_client.set_is_forward(forward)
+        backward = keyboard.is_pressed('/')
+        g_observer_client.set_is_backward(backward)
     except Exception as e:
         logging.exception(e)
-    root.after(10, func)
+    root.after(20, func)
 
 
-root.after(10, func)
+root.after(20, func)
 g_observer_client.start_simulation()
 
 mainloop()
--- ObserverClient.py
+++ ObserverClient.py
@@ -1,5 +1,6 @@
 import time
 from socket import *
+from threading import Thread, Lock
 from typing import Final
 
 from ServerProtocol import *
@@ -9,9 +10,28 @@ class ObserverClient(object):
     def __init__(self):
         # constants
         self.SERVER_IP: Final = '127.0.0.1'
-        # vars
-        self.m_is_simulation_running = False
+        self.addr: Final = None
+        self.EYE_IMAGE_DELAY: Final = 5000 # quantum of time. Eye inertia
+        self.STATISTIC_REQUEST_PERIOD: Final = 900 # milliseconds
 
+        # vars
+        self.isSimulationRunning = False
+        self.timeOfTheUniverse = 0
+        self.eyeTextureMutex = Lock()
+        self.isLeft = self.isRight = self.isUp = self.isDown = self.isForward = self.isBackward = 0
+        # init eye color array
+        self.eyeColorArray = []
+        self.eyeUpdateTimeArray = []
+        for i in range(CommonParams.OBSERVER_EYE_SIZE):
+            row_color = []
+            row_time = []
+            for j in range(CommonParams.OBSERVER_EYE_SIZE):
+                row_color.append(EtherColor(0, 0, 0))
+                row_time.append(0)
+            self.eyeColorArray.append(row_color)
+            self.eyeUpdateTimeArray.append(row_time)
+        # Create a UDP socket
+        self.client_socket = socket(AF_INET, SOCK_DGRAM)
 
     def __new__(cls):
         if not hasattr(cls, 'instance'):
@@ -19,25 +39,24 @@ class ObserverClient(object):
         return cls.instance
 
     def start_simulation(self):
-        self.m_is_simulation_running = True
+        self.isSimulationRunning = True
+        # Set a timeout value of 1 second
+        self.client_socket.settimeout(1)
 
-        for port in range(CommonParams.CLIENT_UDP_PORT_START, CommonParams.CLIENT_UDP_PORT_START+CommonParams.MAX_CLIENTS):
-            # Create a UDP socket
-            client_socket = socket(AF_INET, SOCK_DGRAM)
-            # Set a timeout value of 1 second
-            client_socket.settimeout(1)
-            # Ping to server
-            addr = (self.SERVER_IP, port)
+        for port in range(CommonParams.CLIENT_UDP_PORT_START,
+                          CommonParams.CLIENT_UDP_PORT_START + CommonParams.MAX_CLIENTS):
+            self.client_socket.setblocking(True)
+
+            self.addr = (self.SERVER_IP, port)
             message = MsgCheckVersion()
             message.m_clientVersion = CommonParams.PROTOCOL_VERSION
             message.m_observerId = 0
-            arr = message.get_buffer()
             # Send
-            client_socket.sendto(arr, addr)
+            self.client_socket.sendto(message.get_buffer(), self.addr)
 
             # If data is received back from server, print
             try:
-                data, server = client_socket.recvfrom(CommonParams.DEFAULT_BUFLEN)
+                data = self.client_socket.recv(CommonParams.DEFAULT_BUFLEN)
                 if data[0] == MsgType.CheckVersionResponse:
                     received_array = bytearray(data)
                     msg_receive = MsgCheckVersionResponse.from_buffer(received_array)
@@ -45,22 +64,114 @@ class ObserverClient(object):
                         break
                     else:
                         print('Wrong protocol')
-                        addr = None
+                        self.addr = None
                         break
 
             except timeout:
                 print('REQUEST TIMED OUT')
-                addr = None
+                self.addr = None
 
-        if addr != None:
-            addr = None
+        if self.addr is not None:
+            self.client_socket.setblocking(False)
+            t = Thread(target=self.thread_cycle)
+            t.start()
 
         return
+
+    def stop_simulation(self):
+        if self.isSimulationRunning:
+            # TODO stop thread
+            pass
+
+    def set_is_left(self, value):
+        self.isLeft = value
+
+    def set_is_right(self, value):
+        self.isRight = value
+
+    def set_is_up(self, value):
+        self.isUp = value
+
+    def set_is_down(self, value):
+        self.isDown = value
+
+    def set_is_forward(self, value):
+        self.isForward = value
+
+    def set_is_backward(self, value):
+        self.isBackward = value
+
+    def get_eye_texture(self):
+        eye_color_array = []
+        eye_update_time_array = []
+        with self.eyeTextureMutex:
+            eye_color_array = self.eyeColorArray
+            eye_update_time_array = self.eyeUpdateTimeArray
+        for xx in range(CommonParams.OBSERVER_EYE_SIZE):
+            for yy in range(CommonParams.OBSERVER_EYE_SIZE):
+                time_diff = self.timeOfTheUniverse - eye_update_time_array[yy][xx]
+                alpha = self.eyeColorArray[yy][xx].m_colorA
+                if time_diff < self.EYE_IMAGE_DELAY:
+                    alpha = int((alpha * (self.EYE_IMAGE_DELAY - time_diff) / self.EYE_IMAGE_DELAY))
+                else:
+                    alpha = 0
+                eye_color_array[yy][xx].m_colorA = alpha
+        return eye_color_array
+
+    @staticmethod
+    def get_time_ms():
+        return time.time_ns() // 1000000
+
+    def thread_cycle(self):
+        while self.isSimulationRunning:
+            self.pph_tick()
+
+    def pph_tick(self):
+        message = MsgGetState()
+        self.client_socket.sendto(message.get_buffer(), self.addr)
+        if self.isLeft:
+            message = MsgRotateLeft()
+            message.m_value = 4
+            self.client_socket.sendto(message.get_buffer(), self.addr)
+        if self.isRight:
+            message = MsgRotateRight()
+            message.m_value = 4
+            self.client_socket.sendto(message.get_buffer(), self.addr)
+        if self.isUp:
+            message = MsgRotateDown()
+            message.m_value = 4
+            self.client_socket.sendto(message.get_buffer(), self.addr)
+        if self.isDown:
+            message = MsgRotateUp()
+            message.m_value = 4
+            self.client_socket.sendto(message.get_buffer(), self.addr)
+        if self.isForward:
+            message = MsgMoveForward()
+            message.m_value = 16
+            self.client_socket.sendto(message.get_buffer(), self.addr)
+        if self.isBackward:
+            message = MsgMoveBackward()
+            message.m_value = 16
+            self.client_socket.sendto(message.get_buffer(), self.addr)
+        # receive
+        while True:
+            try:
+                data = self.client_socket.recv(CommonParams.DEFAULT_BUFLEN)
+                if len(data) == 0:
+                    break
+                buf = bytearray(data)
+                if data[0] == MsgType.GetStateResponse:
+                    msg = MsgGetStateResponse.from_buffer(buf)
+                    self.timeOfTheUniverse = msg.m_time
+                elif data[0] == MsgType.SendPhoton:
+                    msg = MsgSendPhoton.from_buffer(buf)
+                    with self.eyeTextureMutex:
+                        self.eyeColorArray[CommonParams.OBSERVER_EYE_SIZE - msg.m_posY - 1][msg.m_posX] = msg.m_color
+                        self.eyeUpdateTimeArray[CommonParams.OBSERVER_EYE_SIZE - msg.m_posY - 1][
+                            msg.m_posX] = self.timeOfTheUniverse
+            except BlockingIOError:
+                break
 
 
 g_observer_client = ObserverClient()
 
-# print("Object created", s)
-# s1 = ObserverClient()
-# print("Object created", s1)
-# ObserverClient().start_simulation()
